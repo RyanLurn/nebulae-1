a8e33611b25ce8f58ccd7a17f92fb257340c7440
Ryan Luong <lumintriet@gmail.com>
2026-07-19 19:50:42 +0700

    feat(result/types): define types.

---
+++ libs/result/src/types.ts
@@ -0,0 +1,15 @@
+import type { AppError } from "@repo/error";
+
+export interface Ok<D> {
+  ok: true;
+  data: D;
+  metadata: unknown;
+}
+
+export interface Err<E extends AppError<string>> {
+  ok: false;
+  error: E;
+  metadata: unknown;
+}
+
+export type Result<D, E extends AppError<string>> = Ok<D> | Err<E>;
