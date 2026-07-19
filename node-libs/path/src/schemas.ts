7730d0b7833c574808028ec55ec672cc87da85ee
Ryan Luong <lumintriet@gmail.com>
2026-07-19 20:28:29 +0700

    feat(node-path/schemas): define AbsolutePathSchema.

---
+++ node-libs/path/src/schemas.ts
@@ -0,0 +1,13 @@
+import { isAbsolute } from "node:path";
+import { z } from "zod";
+
+import type { AbsolutePath } from "@/types";
+
+export const AbsolutePathSchema = z
+  .string("An absolute path must be a string.")
+  .min(1, "An absolute path cannot be empty.")
+  .refine(
+    (value) => isAbsolute(value),
+    "Expected an absolute path, received a relative one.",
+  )
+  .transform((value) => value as AbsolutePath);
