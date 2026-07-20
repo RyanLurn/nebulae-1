b027924c9eaa077b376f6d8846ee767ad6173a2e
Ryan Luong <lumintriet@gmail.com>
2026-07-20 09:08:30 +0700

    feat(docker-client): define constructDockerClient.

---
+++ docker-libs/client/src/constant.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_DOCKER_ENGINE_API_URL = "http://localhost";
+export const DEFAULT_DOCKER_ENGINE_API_VERSION = "v1.55";
+export const DEFAULT_DOCKER_ENGINE_API_SOCKET_PATH = "/var/run/docker.sock";
---
+++ docker-libs/client/src/index.ts
@@ -0,0 +1,19 @@
+import { Client } from "undici";
+
+import {
+  DEFAULT_DOCKER_ENGINE_API_SOCKET_PATH,
+  DEFAULT_DOCKER_ENGINE_API_URL,
+  DEFAULT_DOCKER_ENGINE_API_VERSION,
+} from "@/constant";
+
+export function constructDockerClient(
+  url:
+    | string
+    | URL = `${DEFAULT_DOCKER_ENGINE_API_URL}/${DEFAULT_DOCKER_ENGINE_API_VERSION}`,
+  options: Client.Options = {
+    socketPath: DEFAULT_DOCKER_ENGINE_API_SOCKET_PATH,
+  },
+) {
+  const dockerClient = new Client(url, options);
+  return dockerClient;
+}
