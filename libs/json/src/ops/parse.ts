518c3ad4d4756e4cc0b7237f6b6bbbae87be6282
Ryan Luong <lumintriet@gmail.com>
2026-07-20 10:09:05 +0700

    feat(json/ops): define Result type for parseJsonString operation.

--- libs/json/src/ops/parse.ts
+++ libs/json/src/ops/parse.ts
@@ -1,11 +1,20 @@
+import type { Result } from "@repo/result";
+
 import { UnexpectedError } from "@repo/error";
 import { err, ok } from "@repo/result";
 
-export function parseJsonString(...params: Parameters<typeof JSON.parse>) {
+import { InvalidJsonError } from "@/error/classes/invalid-json";
+
+export function parseJsonString(
+  ...params: Parameters<typeof JSON.parse>
+): Result<any, InvalidJsonError | UnexpectedError> {
   try {
     const value = JSON.parse(...params);
     return ok(value);
   } catch (error) {
+    if (error instanceof SyntaxError) {
+      return err(new InvalidJsonError({ text: params[0], cause: error }));
+    }
     return err(
       new UnexpectedError({
         action: "parse JSON string",
