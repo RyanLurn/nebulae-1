e1c1113b8e2f7715f0a49c500bea16e3e31dfece
Ryan Luong <lumintriet@gmail.com>
2026-07-20 10:44:02 +0700

    feat(docker-images): add error handling to buildDockerImage operation.

--- docker-libs/images/src/ops/build.ts
+++ docker-libs/images/src/ops/build.ts
@@ -1,16 +1,38 @@
 import type { Client } from "undici";
 
+import { UnexpectedError } from "@repo/error";
+import { err, ok } from "@repo/result";
+
 export async function buildDockerImage(
   dockerClient: Client,
   buildContextTar: Buffer,
 ) {
-  const response = await dockerClient.request({
-    path: "/build",
-    method: "POST",
-    query: {
-      version: "2",
-    },
-    body: buildContextTar,
-  });
-  return response;
+  try {
+    const { statusCode, statusText } = await dockerClient.request({
+      path: "/build",
+      method: "POST",
+      query: {
+        version: "2",
+      },
+      body: buildContextTar,
+    });
+
+    if (statusCode === 200) {
+      return ok(null);
+    }
+
+    return err(
+      new UnexpectedError({
+        action: "build Docker image",
+        cause: statusText,
+      }),
+    );
+  } catch (error) {
+    return err(
+      new UnexpectedError({
+        action: "build Docker image",
+        cause: error,
+      }),
+    );
+  }
 }
