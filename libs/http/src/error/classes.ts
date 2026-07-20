7d446dc6a1acb8d83da77268775514511d416002
Ryan Luong <lumintriet@gmail.com>
2026-07-21 06:06:03 +0700

    feat(http/error): define 2 more classes.

--- libs/http/src/error/classes.ts
+++ libs/http/src/error/classes.ts
@@ -3,36 +3,46 @@ import { BaseError } from "@repo/error";
 import type { HttpRequestMethod } from "@/constants/request-method-record";
 import type { HttpErrorCodeRecord } from "@/error/code-record";
 
+import { HTTP_ERROR_CODE_RECORD } from "@/error/code-record";
+
 export abstract class HttpError<
   Key extends keyof HttpErrorCodeRecord,
 > extends BaseError<HttpErrorCodeRecord[Key]["code"]> {
   // oxlint-disable-next-line unicorn/custom-error-definition
   abstract override readonly name: string;
   abstract override readonly code: HttpErrorCodeRecord[Key]["code"];
+  abstract readonly statusCode: HttpErrorCodeRecord[Key]["statusCode"];
+  abstract readonly statusText: HttpErrorCodeRecord[Key]["statusText"];
   readonly url: string;
   readonly method: HttpRequestMethod;
-  readonly statusCode: HttpErrorCodeRecord[Key]["statusCode"];
-  readonly statusText: HttpErrorCodeRecord[Key]["statusText"];
 
   constructor({
     message,
     url,
     method,
-    statusCode,
-    statusText,
     cause,
   }: {
-    message?: string;
+    message: string;
     url: string;
     method: HttpRequestMethod;
-    statusCode: HttpErrorCodeRecord[Key]["statusCode"];
-    statusText: HttpErrorCodeRecord[Key]["statusText"];
     cause: unknown;
   }) {
-    super({ message: message ?? statusText, cause });
+    super({ message, cause });
     this.url = url;
     this.method = method;
-    this.statusCode = statusCode;
-    this.statusText = statusText;
   }
+}
+
+export class HttpBadRequestError extends HttpError<"BAD_REQUEST"> {
+  readonly name = "HttpBadRequestError";
+  readonly code = HTTP_ERROR_CODE_RECORD.BAD_REQUEST.code;
+  readonly statusCode = HTTP_ERROR_CODE_RECORD.BAD_REQUEST.statusCode;
+  readonly statusText = HTTP_ERROR_CODE_RECORD.BAD_REQUEST.statusText;
+}
+
+export class HttpInternalServerError extends HttpError<"INTERNAL_SERVER_ERROR"> {
+  readonly name = "HttpInternalServerError";
+  readonly code = HTTP_ERROR_CODE_RECORD.INTERNAL_SERVER_ERROR.code;
+  readonly statusCode = HTTP_ERROR_CODE_RECORD.INTERNAL_SERVER_ERROR.statusCode;
+  readonly statusText = HTTP_ERROR_CODE_RECORD.INTERNAL_SERVER_ERROR.statusText;
 }
