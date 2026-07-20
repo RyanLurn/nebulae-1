cc4d42c7a3148f0445391b1917c5d5d69fcdc70c
Ryan Luong <lumintriet@gmail.com>
2026-07-20 20:13:31 +0700

    feat(http): define lib entry.

---
+++ libs/http/src/index.ts
@@ -0,0 +1,12 @@
+export type {
+  HttpRequestMethodRecord,
+  HttpRequestMethod,
+} from "@/constants/request-method-record";
+export { HTTP_REQUEST_METHOD_RECORD } from "@/constants/request-method-record";
+
+export type {
+  HttpResponseStatusRecord,
+  HttpResponseStatusCode,
+  HttpResponseStatusText,
+} from "@/constants/response-status-record";
+export { HTTP_RESPONSE_STATUS_RECORD } from "@/constants/response-status-record";
