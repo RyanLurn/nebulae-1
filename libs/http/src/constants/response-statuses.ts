af55b352be9210d84475fc17e2bd8751240200e6
Ryan Luong <lumintriet@gmail.com>
2026-07-20 19:33:33 +0700

    feat(http/constants): declare HTTP_RESPONSE_STATUSES.

---
+++ libs/http/src/constants/response-statuses.ts
@@ -0,0 +1,42 @@
+export const HTTP_RESPONSE_STATUSES = {
+  OK: {
+    code: 200,
+    text: "OK",
+  },
+  CREATED: {
+    code: 201,
+    text: "Created",
+  },
+  NO_CONTENT: {
+    code: 204,
+    text: "No Content",
+  },
+  BAD_REQUEST: {
+    code: 400,
+    text: "Bad Request",
+  },
+  UNAUTHORIZED: {
+    code: 401,
+    text: "Unauthorized",
+  },
+  FORBIDDEN: {
+    code: 403,
+    text: "Forbidden",
+  },
+  NOT_FOUND: {
+    code: 404,
+    text: "Not Found",
+  },
+  CONFLICT: {
+    code: 409,
+    text: "Conflict",
+  },
+  TOO_MANY_REQUESTS: {
+    code: 429,
+    text: "Too Many Requests",
+  },
+  INTERNAL_SERVER_ERROR: {
+    code: 500,
+    text: "Internal Server Error",
+  },
+} as const;
