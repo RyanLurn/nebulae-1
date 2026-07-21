9aeb0f8e7ab396d5b752f9676f8d00823adb07df
Ryan Luong <lumintriet@gmail.com>
2026-07-21 20:19:58 +0700

    feat(docker-error/classes): rewrite classes to use the generic cause.

--- docker-libs/error/src/classes.ts
+++ docker-libs/error/src/classes.ts
@@ -1,30 +1,18 @@
-import type { HttpResponseStatusRecord } from "@repo/http";
-
 import { BaseError } from "@repo/error";
 
-import type { DockerErrorResponseBody } from "@/schemas";
-
-export interface DockerBadParameterErrorCause extends DockerErrorResponseBody {
-  statusCode: HttpResponseStatusRecord["BAD_REQUEST"]["code"];
-  statusText: string;
-}
+import type { DockerErrorCause } from "@/types";
 
 export class DockerBadParameterError extends BaseError<
   "DOCKER_BAD_PARAMETER_ERROR",
-  DockerBadParameterErrorCause
+  DockerErrorCause<400>
 > {
   readonly name = "DockerBadParameterError";
   readonly code = "DOCKER_BAD_PARAMETER_ERROR";
 }
 
-export interface DockerServerErrorCause extends DockerErrorResponseBody {
-  statusCode: HttpResponseStatusRecord["INTERNAL_SERVER_ERROR"]["code"];
-  statusText: string;
-}
-
 export class DockerServerError extends BaseError<
   "DOCKER_SERVER_ERROR",
-  DockerServerErrorCause
+  DockerErrorCause<500>
 > {
   readonly name = "DockerServerError";
   readonly code = "DOCKER_SERVER_ERROR";
