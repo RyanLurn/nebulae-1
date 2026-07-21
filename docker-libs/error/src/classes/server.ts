33e9a1ed01402abdef95168b1a19fb95344002e3
Ryan Luong <lumintriet@gmail.com>
2026-07-21 19:21:57 +0700

    feat(docker-error/classes): rewrite DockerServerError class.

--- docker-libs/error/src/classes/server.ts
+++ docker-libs/error/src/classes/server.ts
@@ -1,11 +1,36 @@
+import type { HttpResponseStatusRecord } from "@repo/http";
+
 import { BaseError } from "@repo/error";
+import { HTTP_RESPONSE_STATUS_RECORD } from "@repo/http";
 
 import type { DockerErrorResponseBody } from "@/schemas";
 
+export interface DockerServerErrorCause extends DockerErrorResponseBody {
+  statusCode: HttpResponseStatusRecord["INTERNAL_SERVER_ERROR"]["code"];
+  statusText: "server error";
+}
+
 export class DockerServerError extends BaseError<
   "DOCKER_SERVER_ERROR",
-  DockerErrorResponseBody
+  DockerServerErrorCause
 > {
   readonly name = "DockerServerError";
   readonly code = "DOCKER_SERVER_ERROR";
+
+  constructor({
+    message,
+    responseBody,
+  }: {
+    message: string;
+    responseBody: DockerErrorResponseBody;
+  }) {
+    super({
+      message,
+      cause: {
+        message: responseBody.message,
+        statusCode: HTTP_RESPONSE_STATUS_RECORD.INTERNAL_SERVER_ERROR.code,
+        statusText: "server error",
+      },
+    });
+  }
 }
