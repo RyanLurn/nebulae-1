dd17ead13cebe68b74afe8d81693f4af209e881c
Ryan Luong <lumintriet@gmail.com>
2026-07-21 20:02:57 +0700

    feat(http/constants): make status record modular.

--- libs/http/src/constants/response-status-record.ts
+++ libs/http/src/constants/response-status-record.ts
@@ -1,4 +1,4 @@
-export const HTTP_RESPONSE_STATUS_RECORD = {
+export const HTTP_SUCCESSFUL_RESPONSE_STATUS_RECORD = {
   OK: {
     code: 200,
     text: "OK",
@@ -11,6 +11,15 @@ export const HTTP_RESPONSE_STATUS_RECORD = {
     code: 204,
     text: "No Content",
   },
+} as const;
+export type HttpSuccessfulResponseStatusRecord =
+  typeof HTTP_SUCCESSFUL_RESPONSE_STATUS_RECORD;
+export type HttpSuccessfulResponseStatusCode =
+  HttpSuccessfulResponseStatusRecord[keyof HttpSuccessfulResponseStatusRecord]["code"];
+export type HttpSuccessfulResponseStatusText =
+  HttpSuccessfulResponseStatusRecord[keyof HttpSuccessfulResponseStatusRecord]["text"];
+
+export const HTTP_CLIENT_ERROR_RESPONSE_STATUS_RECORD = {
   BAD_REQUEST: {
     code: 400,
     text: "Bad Request",
@@ -35,16 +44,48 @@ export const HTTP_RESPONSE_STATUS_RECORD = {
     code: 429,
     text: "Too Many Requests",
   },
+} as const;
+export type HttpClientErrorResponseStatusRecord =
+  typeof HTTP_CLIENT_ERROR_RESPONSE_STATUS_RECORD;
+export type HttpClientErrorResponseStatusCode =
+  HttpClientErrorResponseStatusRecord[keyof HttpClientErrorResponseStatusRecord]["code"];
+export type HttpClientErrorResponseStatusText =
+  HttpClientErrorResponseStatusRecord[keyof HttpClientErrorResponseStatusRecord]["text"];
+
+export const HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD = {
   INTERNAL_SERVER_ERROR: {
     code: 500,
     text: "Internal Server Error",
   },
+  SERVICE_UNAVAILABLE: {
+    code: 503,
+    text: "Service Unavailable",
+  },
 } as const;
+export type HttpServerErrorResponseStatusRecord =
+  typeof HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD;
+export type HttpServerErrorResponseStatusCode =
+  HttpServerErrorResponseStatusRecord[keyof HttpServerErrorResponseStatusRecord]["code"];
+export type HttpServerErrorResponseStatusText =
+  HttpServerErrorResponseStatusRecord[keyof HttpServerErrorResponseStatusRecord]["text"];
 
+export const HTTP_ERROR_RESPONSE_STATUS_RECORD = {
+  ...HTTP_CLIENT_ERROR_RESPONSE_STATUS_RECORD,
+  ...HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD,
+} as const;
+export type HttpErrorResponseStatusRecord =
+  typeof HTTP_ERROR_RESPONSE_STATUS_RECORD;
+export type HttpErrorResponseStatusCode =
+  HttpErrorResponseStatusRecord[keyof HttpErrorResponseStatusRecord]["code"];
+export type HttpErrorResponseStatusText =
+  HttpErrorResponseStatusRecord[keyof HttpErrorResponseStatusRecord]["text"];
+
+export const HTTP_RESPONSE_STATUS_RECORD = {
+  ...HTTP_SUCCESSFUL_RESPONSE_STATUS_RECORD,
+  ...HTTP_ERROR_RESPONSE_STATUS_RECORD,
+} as const;
 export type HttpResponseStatusRecord = typeof HTTP_RESPONSE_STATUS_RECORD;
-
 export type HttpResponseStatusCode =
   HttpResponseStatusRecord[keyof HttpResponseStatusRecord]["code"];
-
 export type HttpResponseStatusText =
   HttpResponseStatusRecord[keyof HttpResponseStatusRecord]["text"];
