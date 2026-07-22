a34a4e85cb87105917e812f20faeb5670cd92cde
Ryan Luong <lumintriet@gmail.com>
2026-07-22 08:00:25 +0700

    feat(docker-images/list): define ListImagesEndpointFiltersQueryParamSchema.

--- docker-libs/images/src/ops/list-images.ts
+++ docker-libs/images/src/ops/list-images.ts
@@ -1,8 +1,24 @@
 import { z } from "zod";
 
-export const ListImagesEndpointQueryParamsSchema = z.object({
-  all: z.boolean().default(false).optional(),
-});
+export const ListImagesEndpointFiltersQueryParamSchema = z
+  .object({
+    before: z.array(z.string()),
+    dangling: z.array(z.union([z.literal("true"), z.literal("false")])),
+    label: z.array(z.string()),
+    reference: z.array(z.string()),
+    since: z.array(z.string()),
+  })
+  .partial();
+export type ListImagesEndpointFiltersQueryParam = z.infer<
+  typeof ListImagesEndpointFiltersQueryParamSchema
+>;
+
+export const ListImagesEndpointQueryParamsSchema = z
+  .object({
+    all: z.boolean().default(false),
+    filters: ListImagesEndpointFiltersQueryParamSchema,
+  })
+  .partial();
 export type ListImagesEndpointQueryParams = z.infer<
   typeof ListImagesEndpointQueryParamsSchema
 >;
