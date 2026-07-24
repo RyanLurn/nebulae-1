ea0e0a375286a9052fc10f20a3836f99a781309f
Ryan Luong <lumintriet@gmail.com>
2026-07-24 20:55:50 +0700

    feat(openapi-spec/schemas): declare OpenApiLongSchema.

--- openapi-libs/spec/src/schemas/primitives.ts
+++ openapi-libs/spec/src/schemas/primitives.ts
@@ -2,3 +2,6 @@ import { z } from "zod";
 
 export const OpenApiIntegerSchema = z.int32();
 export type OpenApiInteger = z.infer<typeof OpenApiIntegerSchema>;
+
+export const OpenApiLongSchema = z.int64();
+export type OpenApiLong = z.infer<typeof OpenApiLongSchema>;
